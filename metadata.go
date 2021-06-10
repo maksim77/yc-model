82ff8e2981d6bc1f5629f0088c23ff868b3093d2
Maksim Syomochkin <maksim77ster@gmail.com>
2021-06-10 17:01:20 +0300

    add TracingContext to metadata object

--- metadata.go
+++ metadata.go
@@ -3,9 +3,16 @@ package ycmodel
 import "time"
 
 type EventMetadata struct {
-	EventID   string    `json:"event_id"`
-	EventType string    `json:"event_type"`
-	CreatedAt time.Time `json:"created_at"`
-	CloudID   string    `json:"cloud_id"`
-	FolderID  string    `json:"folder_id"`
+	EventID        string         `json:"event_id"`
+	EventType      string         `json:"event_type"`
+	CreatedAt      time.Time      `json:"created_at"`
+	CloudID        string         `json:"cloud_id"`
+	FolderID       string         `json:"folder_id"`
+	TracingContext TracingContext `json:"tracing_context"`
+}
+
+type TracingContext struct {
+	TraceID       string `json:"trace_id"`
+	SpanID        string `json:"span_id"`
+	ParrentSpanID string `json:"parent_span_id"`
 }
